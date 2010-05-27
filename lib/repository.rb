b3c0b101988012c4a3fa84c4bf99c71b70b71231
Darrin Holst <darrinholst@gmail.com>
2010-05-27 13:47:07 -0500

    fixes for new restclient

--- lib/repository.rb
+++ lib/repository.rb
@@ -8,48 +8,48 @@ class Nexus::Repository
     :name => :a,
     :type => :p
   }
-  
+
   def initialize(baseuri, username = nil, password = nil)
     @baseuri = baseuri
     @username = username
     @password = password
   end
-  
+
   def find_artifacts(args)
     url = "#{@baseuri}/service/local/data_index/repo_groups/public?#{build_query_parameters_from(args)}"
     response = RestClient.get(url, :accept => 'application/json')
-    Crack::JSON.parse(response)['data'].map {|data| Nexus::Artifact.new(data)}
+    Crack::JSON.parse(response.body)['data'].map {|data| Nexus::Artifact.new(data)}
   end
-  
+
   def download(artifact)
     RestClient.get(artifact.uri)
   end
-  
+
   def delete(artifact)
     url = "#{@baseuri}/service/local/repositories/#{artifact.repo}/content/#{artifact.group.gsub(/\./, '/')}/#{artifact.name}/#{artifact.version}/"
     RestClient::Resource.new(url, :user => @username, :password => @password).delete.code
   end
-  
+
   private
-  
+
   def build_query_parameters_from(args)
     raise "arguments must be a hash" unless args.is_a? Hash
 
     query_parameters = args.map do |entry|
       "#{nexus_parameter_for(entry.first)}=#{entry.last}"
     end.join("&")
-    
+
     raise "must pass at least one argument #{nexus_parameters_to_s}" if query_parameters.empty?
-    
+
     query_parameters
   end
-  
+
   def nexus_parameter_for(parameter)
     nexus_param = NEXUS_PARAMETERS[parameter]
     raise "invalid argument passed, valid arguments are #{nexus_parameters_to_s}" unless nexus_param
     nexus_param
   end
-  
+
   def nexus_parameters_to_s
     "(#{NEXUS_PARAMETERS.keys.join(', ')})"
   end
--- test/repository_test.rb
+++ test/repository_test.rb
@@ -21,28 +21,28 @@ class RepositoryTest < Test::Unit::TestCase
        ]
     }
   eos
-  
+
   def setup
     @repository = Nexus::Repository.new("http://localhost/nexus", "user", "pass")
   end
-  
+
   def test_find_artifacts_using_all_options
     expected_url = "http://localhost/nexus/service/local/data_index/repo_groups/public?a=artifact&g=group&p=war"
     FakeWeb.register_uri(:get, expected_url, :body => JSON)
     assert_artifacts @repository.find_artifacts(:group => 'group', :name => 'artifact', :type => 'war')
   end
-  
+
   def test_find_artifacts_using_artifact_id_only
     expected_url = "http://localhost/nexus/service/local/data_index/repo_groups/public?a=artifact"
     FakeWeb.register_uri(:get, expected_url, :body => JSON)
-    assert_artifacts @repository.find_artifacts(:name => 'artifact')    
+    assert_artifacts @repository.find_artifacts(:name => 'artifact')
   end
-  
+
   def test_find_artifacts_when_arguments_is_not_a_hash
     exception = assert_raise RuntimeError do
       @repository.find_artifacts(nil)
     end
-    
+
     assert_equal("arguments must be a hash", exception.message)
   end
 
@@ -50,38 +50,38 @@ class RepositoryTest < Test::Unit::TestCase
     exception = assert_raise RuntimeError do
       @repository.find_artifacts(:a => 'foo', :b => 'bar')
     end
-    
+
     assert_equal("invalid argument passed, valid arguments are (type, name, group)", exception.message)
   end
-  
+
   def test_find_artifacts_when_empty_arguments_passed
     exception = assert_raise RuntimeError do
       @repository.find_artifacts({})
     end
-    
+
     assert_equal("must pass at least one argument (type, name, group)", exception.message)
   end
-  
+
   def test_delete
     expected_url = "http://user:pass@localhost/nexus/service/local/repositories/repo/content/org/group/artifact/version/"
     artifact = Nexus::Artifact.new('groupId' => 'org.group', 'artifactId' => 'artifact', 'version' => 'version', 'repoId' => 'repo')
     FakeWeb.register_uri(:delete, expected_url, :status => 200)
     assert_equal(200, @repository.delete(artifact))
   end
-  
+
   def test_download
     artifact = Nexus::Artifact.new('resourceURI' => 'http://nexus/artifact')
     FakeWeb.register_uri(:get, "http://nexus/artifact", :body => 'file content')
     file = @repository.download(artifact)
-    assert_equal('file content', file)
+    assert_equal('file content', file.body)
   end
-  
+
   def assert_artifacts(artifacts)
     assert_equal(2, artifacts.size)
     assert_artifact(artifacts[0], 'group1', 'artifact1', 'version1', 'packaging1', 'uri1')
     assert_artifact(artifacts[1], 'group2', 'artifact2', 'version2', 'packaging2', 'uri2')
   end
-  
+
   def assert_artifact(artifact, group, name, version, type, uri)
     assert_equal(group, artifact.group)
     assert_equal(name, artifact.name)
