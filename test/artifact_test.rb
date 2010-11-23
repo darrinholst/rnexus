ec66284bce4bec9157289403154602474509e8e2
Mike Dalessio <mike@csa.net>
2010-11-23 14:22:24 -0500

    implement artifact == operator

--- test/artifact_test.rb
+++ test/artifact_test.rb
@@ -1,20 +1,41 @@
 require 'test_helper'
 
 class ArtifactTest < Test::Unit::TestCase
-  def test_construction_from_a_hash
-    artifact = Nexus::Artifact.new(
+  def setup
+    @hash = {
       'groupId' => 'group', 
       'artifactId' => 'name', 
       'version' => 'version', 
       'packaging' => 'war', 
       'resourceURI' => 'uri',
-      'repoId' => 'repo')
-      
+      'repoId' => 'repo',
+      'classifier' => 'classifier'
+    }
+  end
+
+  def test_construction_from_a_hash
+    artifact = Nexus::Artifact.new @hash
+
     assert_equal('group', artifact.group)
     assert_equal('name', artifact.name)
     assert_equal('version', artifact.version)
     assert_equal('war', artifact.type)
     assert_equal('uri', artifact.uri)
     assert_equal('repo', artifact.repo)
+    assert_equal('classifier', artifact.classifier)
   end  
+
+  def test_serialization_to_a_hash
+    artifact = Nexus::Artifact.new @hash
+    assert_equal @hash, artifact.to_hash
+  end
+
+  def test_serialization_to_a_hash_roundtrips
+    artifact = Nexus::Artifact.new @hash
+    assert_equal artifact, Nexus::Artifact.new(artifact.to_hash)
+  end
+
+  def test_artifact_equality
+    assert(Nexus::Artifact.new(@hash) == Nexus::Artifact.new(@hash))
+  end
 end
